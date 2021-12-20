3972c002a0e2326b25f86f922cd580e813242ecb
xyzchz <xyzchz@outlook.com>
2021-12-20 23:54:10 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import './set-public-path';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import singleSpaVue from 'single-spa-vue';
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h) {
+      return h(App);
+    },
+    router,
+  },
+});
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
---
+++ src/set-public-path.js
@@ -0,0 +1,2 @@
+import { setPublicPath } from 'systemjs-webpack-interop';
+setPublicPath('bench');
